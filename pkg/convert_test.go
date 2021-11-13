b18f369455bdf7f1b4beb01d3f79ef02cd4b8fe2
Omer Kaya <omerkaya513@gmail.com>
2021-11-13 15:00:29 +0300

    [Add] Initial project layout:
    1) Introduced an extendable project layout;
    2) Moved all exported code into the pkg package.

---
+++ pkg/convert_test.go
@@ -0,0 +1,45 @@
+package pkg_test
+
+import (
+	"testing"
+)
+
+func TestConvertJSONBytes(t *testing.T) {
+}
+
+func BenchmarkConvertJSONBytes(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+
+	}
+}
+
+func TestConvertJSONBytesContext(t *testing.T) {
+}
+
+func BenchmarkConvertJSONBytesContext(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+
+	}
+}
+
+func TestConvertJSONString(t *testing.T) {
+}
+
+func BenchmarkConvertJSONString(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+
+	}
+}
+
+func TestConvertJSONStringContext(t *testing.T) {
+}
+
+func BenchmarkConvertJSONStringContext(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+
+	}
+}
